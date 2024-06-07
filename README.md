b8d800ca51a6b9c6ec8224d11b1053363b1b30f9
Isaac McQueen <imcqueen@truehomesusa.com>
2024-06-06 23:15:26 -0400

    add tmux

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 
 New machine setup
 
-```gh auth login```
+1. Clone repo
+2. sudo nixos-rebuild switch --flake /home/imcquee/nix-home/#nixos
+
+### TODO
+
+-[ ] Migrate Darwin over
+-[ ] Make public
+-[x] Migrate TMUX
+-[ ] Migrate Neovim
--- home.nix
+++ home.nix
@@ -5,9 +5,29 @@
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
 
-  programs.git = {
-    enable = true;
-    userName  = "Isaac McQueen";
-    userEmail = "imcqueen@truehomesusa.com";
+  programs = {
+    git = {
+      enable = true;
+      userName  = "Isaac McQueen";
+      userEmail = "imcqueen@truehomesusa.com";
+    };
+
+    tmux = {
+      enable = true;
+      extraConfig = ''
+      set -g mouse on
+      setw -g mode-keys vi
+      set -sg escape-time 0
+      set-option -g status-position top
+      set-option -g focus-events on
+    
+      # Enable true color support
+      set -g default-terminal "tmux-256color"
+      set -as terminal-features ",*:RGB"
+      # Pop up windows
+      bind -n M-g display-popup -d "#{pane_current_path}" -w 90% -h 90% -E 'lazygit'
+      bind -n M-d display-popup -d "#{pane_current_path}" -w 90% -h 90% -E 'lazydocker'
+      '';
+    };
   };
 }
