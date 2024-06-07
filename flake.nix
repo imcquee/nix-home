d5e7012839d2c302ff5142ef4b3109dee998ee0d
Isaac McQueen <imcqueen@truehomesusa.com>
2024-06-06 22:57:13 -0400

    add path

--- flake.nix
+++ flake.nix
@@ -11,12 +11,14 @@
     # Please replace my-nixos with your hostname
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = inputs;
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
 	./hardware-configuration.nix
 	./nvidia.nix
+	./path.nix
 	home-manager.nixosModules.home-manager
         {
 	  home-manager.useGlobalPkgs = true;
