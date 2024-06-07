13c95a567b66fa07253fc78bb2abe5e1084aa8a3
Isaac McQueen <imcqueen@truehomesusa.com>
2024-06-07 09:13:40 -0400

    add nvim

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/oil.nvim",
+  opts = {
+    view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+    },
+  },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+}
