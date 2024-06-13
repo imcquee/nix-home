9cf22ef1bae4b9cb3b2a41cc63fdb7d484e447ec
Isaac McQueen <imcqueen@truehomesusa.com>
2024-06-12 21:57:29 -0400

    fixes

--- nvidia.nix
+++ nvidia.nix
@@ -45,7 +45,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.production;
 
     # prime = {
     #   # offload = {
