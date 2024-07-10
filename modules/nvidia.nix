e1b79250d9f632681730b8937f7fe608123274f8
Isaac McQueen <imcqueen@truehomesusa.com>
2024-07-10 07:57:53 -0400

    use latest nvidia

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -43,7 +43,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.latest;
 
     # prime = {
     #   # offload = {
