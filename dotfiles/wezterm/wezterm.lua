f16cd085dad30306cd77d2c6740460e24edcdc70
Isaac McQueen <imcqueen@truehomesusa.com>
2024-08-02 09:10:14 -0400

    add font

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -1,5 +1,9 @@
+local wezterm = require("wezterm")
+
 return {
-  enable_tab_bar = false,
-  window_background_opacity = 0.7,
-  macos_window_background_blur = 20,
+	enable_tab_bar = false,
+	window_background_opacity = 0.7,
+	macos_window_background_blur = 20,
+	font = wezterm.font("MartianMono Nerd Font"),
+	font_size = 12.0,
 }
