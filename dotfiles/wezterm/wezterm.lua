36983a20a10b96bd4e7ea1a32328daf022d235f4
Isaac McQueen <imcqueen@truehomesusa.com>
2024-08-02 22:01:25 -0400

    just use bundled

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -4,6 +4,6 @@ return {
 	enable_tab_bar = false,
 	window_background_opacity = 0.7,
 	macos_window_background_blur = 20,
-	font = wezterm.font("MartianMono Nerd Font"),
+	font = wezterm.font("JetBrains Mono"),
 	font_size = 12.0,
 }
