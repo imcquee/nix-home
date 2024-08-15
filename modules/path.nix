41d12a74d36e6dae112c9abb29fd38ca5099eaf7
imcquee <imcqueen@truehomesusa.com>
2024-08-15 08:53:18 -0400

    Update path.nix

--- modules/path.nix
+++ modules/path.nix
@@ -2,6 +2,7 @@
 {
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
   # remove nix-channel related tools & configs, we use flakes instead.
+	# Use flake for nix_path https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
   nix =
     if pkgs.stdenv.isDarwin then
       {
@@ -14,9 +15,5 @@
         registry.nixpkgs.flake = nixpkgs;
         settings.nix-path = nixpkgs.lib.mkForce "nixpkgs=/etc/nix/inputs/nixpkgs";
       };
-
-  # but NIX_PATH is still used by many useful tools, so we set it to the same value as the one used by this flake.
-  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
   environment.etc."nix/inputs/nixpkgs".source = "${nixpkgs}";
-  # https://github.com/NixOS/nix/issues/9574
 }
