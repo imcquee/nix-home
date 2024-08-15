06868f2437da1f8f0091ffac1db0c514bcd33cc4
imcquee <imcqueen@truehomesusa.com>
2024-08-15 08:53:30 -0400

    Update path.nix

--- modules/path.nix
+++ modules/path.nix
@@ -2,7 +2,7 @@
 {
   # make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
   # remove nix-channel related tools & configs, we use flakes instead.
-	# Use flake for nix_path https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
+  # Use flake for nix_path https://nixos-and-flakes.thiscute.world/best-practices/nix-path-and-flake-registry
   nix =
     if pkgs.stdenv.isDarwin then
       {
