da4d429bc6d4c9e6b973099ed2d7d28cb4968fdd
Isaac McQueen <imcqueen@truehomesusa.com>
2024-08-16 08:32:59 -0400

    fix rebuild

--- dotfiles/fish/functions/rebuild.fish
+++ dotfiles/fish/functions/rebuild.fish
@@ -4,5 +4,5 @@ function rebuild
         return 1
     end
     set config $argv[1]
-    sudo nixos-rebuild switch --flake /home/imcquee/nix-home/#$config
+    sudo nixos-rebuild switch --flake /home/$USER/nix-home/#$config
 end
