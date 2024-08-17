692afe4592263f608942bb19ad211cd337a1f7ff
Isaac McQueen <imcqueen@truehomesusa.com>
2024-08-17 08:25:46 -0400

    fish func

--- dotfiles/fish/functions/rebuild.fish
+++ dotfiles/fish/functions/rebuild.fish
@@ -4,5 +4,12 @@ function rebuild
         return 1
     end
     set config $argv[1]
-    sudo nixos-rebuild switch --flake /home/$USER/nix-home/#$config
+
+    if test -e /etc/NIXOS
+        sudo nixos-rebuild switch --flake /home/$USER/nix-home/#$config
+    else if string match -q Darwin (uname)
+        darwin-rebuild switch --flake /home/$USER/nix-home#$config
+    else
+        home-manager switch --flake /home/$USER/nix-home#$config
+    end
 end
