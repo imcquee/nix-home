724672ba41d0afdb779065b5e16b66a29d347128
Isaac McQueen <imcqueen@truehomesusa.com>
2024-08-20 14:56:33 -0400

    no longer needed

--- dotfiles/fish/functions/fish_user_key_bindings.fish
+++
@@ -1,8 +0,0 @@
-function fish_user_key_bindings
-    #Need to use commandline to complete the execution and return a new line
-    bind \ey -M insert 'yy; commandline -f execute'
-    bind \ey 'yy; commandline -f execute'
-
-    bind \eg -M insert 'lazygit; commandline -f execute'
-    bind \eg 'lazygit; commandline -f execute'
-end
