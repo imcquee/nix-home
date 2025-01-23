779afe347c607f7fed3e24afb0951118e6a42d6a
Isaac McQueen <imcqueen@truehomesusa.com>
2025-01-23 04:13:15 -0500

    remove to add back submod

--- dotfiles/yazelix/yazi/sidebar/init.lua
+++
@@ -1,8 +0,0 @@
-function Status:render(area)
-	self.area = area
-
-	local line = ui.Line { self:percentage(), self:position() }
-	return {
-		ui.Paragraph(area, { line }):align(ui.Paragraph.CENTER),
-	}
-end
