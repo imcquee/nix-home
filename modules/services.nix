26ccf5e3a70b8df643a165ad47b7b7ea6dc636a8
Isaac McQueen <imcqueen@truehomesusa.com>
2025-02-04 15:48:51 -0500

    disable ollama for now

--- modules/services.nix
+++ modules/services.nix
@@ -13,17 +13,17 @@
   services.tailscale.enable = true;
 
   # Enable Ollama
-  services.ollama = {
-    enable = true;
-    acceleration = "cuda";
-  };
-
-  # Enable OpenWebUI
-  services.open-webui = {
-    enable = true;
-    host = "0.0.0.0";
-    port = 8080;
-  };
+  # services.ollama = {
+  #   enable = true;
+  #   acceleration = "cuda";
+  # };
+  #
+  # # Enable OpenWebUI
+  # services.open-webui = {
+  #   enable = true;
+  #   host = "0.0.0.0";
+  #   port = 8080;
+  # };
 
   # Enable Nginx
   services.nginx = {
